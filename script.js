5ffdee4eec34f55f4da814526831eb75c09b0b7a
Funmi77 <vrcreativeagency@gmail.com>
2025-07-17 20:30:35 +0100

    Removed the Function for theme button

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-function changeTheme() {
-  let body = document.querySelector("body");
-  if (body.classList.contains("dark")) {
-    body.classList.remove("dark");
-  } else {
-    body.classList.add("dark");
-  }
-}
+let apiKey = "7e77fbbbab91e5504tfaaa75643of118";
+let prompt = "What is the best joke in the world?";
+let context = "Provide clear and precise answer. One joke at a time.";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+axios.get(apiUrl).then(showAnswer);
 
 let themeButton = document.querySelector(".theme-button");
 themeButton.addEventListener("click", changeTheme);
