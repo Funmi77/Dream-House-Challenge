9af8d06fc8dcfad13ea2898428a82bb8acdc2363
Funmi77 <vrcreativeagency@gmail.com>
2025-07-18 19:30:26 +0100

    Updated with Change Theme button

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+function changeTheme() {
+  let body = document.querySelector("body");
+  if (body.classList.contains("dark")) {
+    body.classList.remove("dark");
+  } else {
+    body.classList.add("dark");
+  }
+}
+
 let apiKey = "7e77fbbbab91e5504tfaaa75643of118";
-let prompt = "What is the best joke in the world?";
-let context = "Provide clear and precise answer. One joke at a time.";
+let prompt =
+  "Give me the best Architectural description of a Retro Modern Dream Home";
+let context =
+  "Provide clear and precise answer in just four lines.Make it four lines ";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 axios.get(apiUrl).then(showAnswer);
 
-let containerElement = document.querySelector("#house");
+let themeButton = document.querySelector(".theme-button");
+themeButton.addEventListener("click", changeTheme);
