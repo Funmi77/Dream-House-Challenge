444315987bab2ab1a270914b237bfe4097ab0b69
Funmi77 <vrcreativeagency@gmail.com>
2025-07-18 03:25:05 +0100

    Added typewriter link

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-
-
-
-
-
-
-
 let apiKey = "7e77fbbbab91e5504tfaaa75643of118";
 let prompt = "What is the best joke in the world?";
 let context = "Provide clear and precise answer. One joke at a time.";
@@ -12,7 +5,4 @@ let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${
 
 axios.get(apiUrl).then(showAnswer);
 
-let themeButton = document.querySelector(".theme-button");
-themeButton.addEventListener("click", changeTheme);
-let loveArtButton = document.querySelector("button:not(.theme-button)");
-loveArtButton.addEventListener("click", LoveArt);
+let containerElement = document.querySelector("#house");
