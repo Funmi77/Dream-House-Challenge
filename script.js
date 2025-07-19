2a9efcf4ea959cc5ac029125f734e6e077f11a41
Funmi77 <vrcreativeagency@gmail.com>
2025-07-19 05:34:20 +0100

    Updated Function

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-function changeTheme() {
-  let body = document.querySelector("body");
-  if (body.classList.contains("dark")) {
-    body.classList.remove("dark");
-  } else {
-    body.classList.add("dark");
-  }
+function showAnswer(response) {
+  console.log(response.data.answer);
+  let details = document.querySelector("#details");
+  details.innerHTML = "";
+
+  new Typewriter("#details", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
 }
 
 let apiKey = "7e77fbbbab91e5504tfaaa75643of118";
 let prompt =
-  "Give me the best Architectural description of a Retro Modern Dream Home";
+  "Give me the best Architectural description of a Retro Modern Dream Home.";
 let context =
-  "Provide clear and precise answer in just four lines.Make it four lines ";
+  "Provide clear and precise answer in just four lines.Make it four lines.";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-axios.get(apiUrl).then(showAnswer);
-
-let themeButton = document.querySelector(".theme-button");
-themeButton.addEventListener("click", changeTheme);
+let button = document.querySelector("#getDetails");
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  axios.get(apiUrl).then(showAnswer);
+});
